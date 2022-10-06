f4a69174401423bd93e079cfbbc9636a76855f14
гк <lisitsa13ht@gmail.com>
2022-10-06 20:12:17 +0300

    Initial commit

---
+++ src/com/company/Taylor.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Taylor {
+    public static void main(String[] args) {
+        double x = Math.PI/6;
+        double y = Math.sin(x);
+        System.out.println(y);
+        double eps =1e-5, sum=x, a=x;
+        while (a)
+
+
+
+    }
+}
